ef75a93fb840b6be4997dbfc5fce51ce70a54d43
amandine-duvanel <87307030+amandine-duvanel@users.noreply.github.com>
2021-07-28 13:25:44 +0200

    exo javascript ok

--- FRONT/DYNAMIQUE/JAVASCRIPT/exo 10/10.2/javascript.js
+++ FRONT/DYNAMIQUE/JAVASCRIPT/exo 10/10.2/javascript.js
@@ -1,29 +1,73 @@
-//GetInteger = lire un entier au clavier
-// demande a l' utilisateur combien de cases dans le tableau
+// faut il déclarer tableau en globale ?
 
-function GetInteger(){
+var max = 0;
+var moyenne = 0;
+var size = 0;
+var saisie;
 
-    var size = parseint(prompt("nombre d'entrées dans le tableau ?"));
-    if( !isNaN(size) && (size > 0) ){
+//GetInteger = lire un entier au clavier = combien de cases dans le tableau ?
+
+function GetInteger() {
+
+    size = parseInt(prompt("nombre d'entrées dans le tableau ?"));
+
+    if (Number.isInteger(size)) {
         return size;
-    } else return;
+    }
 }
+taille = GetInteger()
 
 //IniTab = pour créer et initialiser l'instance de tableau (nmbr post saisis au clavier)
 
 //return un tableau de la taille size
-function InitTab(taille){
-    var size = parseInt(taille);
-    if( !isNaN(size) && (size > 0)){
-       var tableau = new Array(size);
-        return tableau;
-    } else return;
+function InitTab() {
+
+    tableau = new Array(taille);
+    return tableau;
+}
 
 //SaisieTab = saisie des différents posts du tableau
+function SaisieTab() {
+    for (var i = 0; i < taille; i++) {
+        tableau[i] = parseInt(prompt('Veuillez entrer un post : '));
+    }
+}
 
-// AfficheTab = affciher tous les posts du tab
+// AfficheTab = afficher tous les posts du tab
+function AfficheTab() {
+
+    for (var i = 0; i < taille; i++) {
+        num = i + 1
+        document.write("Le poste N° " + num + " correspond à : " + tableau[i] + "</br>");
+    }
+}
 
 // RechercheTab afficher le contenu d'un post dont le rang est saisie au clavier
+function RechercheTab() {
+    var selection = parseInt(prompt("veuillez entrer le rang de la cellule que vous voulez voir"));
+    if (selection < tableau.length) {
+        document.write("</br> La cellule au rang " + selection + " est : " + tableau[selection]);
+    } else {
+        alert("L'indice est supérieur au nombre de cellules renseignés !");
+        selection = parseInt(prompt("Veuillez rentrer un autre rang"));
+    }
+}
 
 //InfoTab affiche le max et la moyenne des postes
+function InfoTab() {
+    var somme = 0;
+    for (var j = 0; j < tableau.length; j++) {
+        somme = somme + tableau[j];
+    }
+    moyenne = somme / tableau.length
+    document.write("</br> La moyenne des posts est : " + moyenne);
+    document.write(" </br> Le chiffre maximum du tableau est : " + Math.max(...tableau));
+}
+
+//GetInteger();
+InitTab();
+SaisieTab();
+AfficheTab();
+RechercheTab();
+InfoTab();
 
---
+++ FRONT/DYNAMIQUE/JAVASCRIPT/exo 10/10.3/javascript.js
@@ -0,0 +1,72 @@
+var max = 0;
+var moyenne = 0;
+var size = 0;
+var saisie;
+var tableau = Array;
+
+//GetInteger = lire un entier au clavier = combien de cases dans le tableau ?
+
+function GetInteger() {
+
+    size = parseInt(prompt("nombre d'entrées dans le tableau ?"));
+
+    if (Number.isInteger(size)) {
+        return size;
+    }
+}
+taille = GetInteger()
+
+//IniTab = pour créer et initialiser l'instance de tableau (nmbr post saisis au clavier)
+
+//return un tableau de la taille size
+function InitTab() {
+
+    tableau = new Array(taille);
+    return tableau;
+}
+InitTab();
+
+
+//SaisieTab = saisie des différents posts du tableau
+function SaisieTab() {
+    for (var i = 0; i < taille; i++) {
+        tableau[i] = parseInt(prompt('Veuillez entrer un post : '));
+    }
+}
+SaisieTab();
+
+
+// AfficheTab = afficher tous les posts du tab
+function AfficheTab() {
+
+    for (var i = 0; i < taille; i++) {
+        num = i + 1
+        document.write("Le poste N° " + num + " correspond à : " + tableau[i] + "</br>");
+    }
+}
+
+AfficheTab();
+
+// tri à bulles
+// utiliser while ou do...while ? > essai avec while
+
+function tri(tableau){
+    var condition;
+    do{ 
+        condition = false;
+        for(var i = 0;i<tableau.length;i++) {
+            if (tableau[i]>tableau[i+1]){
+                    var stock = tableau[i];
+                    tableau[i]=tableau[i+1];
+                    tableau[i+1]=stock;
+                    condition = true;
+
+            }
+        }
+    }
+    while (condition);
+}
+
+tri(tableau);
+console.log(tableau);
+document.write("le tableau trié : " + tableau)
