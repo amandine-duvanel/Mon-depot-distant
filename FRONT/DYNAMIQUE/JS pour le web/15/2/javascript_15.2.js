5b62736dfb86ba6e90047234525a83e10bcee7be
amandine-duvanel <87307030+amandine-duvanel@users.noreply.github.com>
2021-07-30 10:41:14 +0200

    save js 15

--- FRONT/DYNAMIQUE/JS pour le web/15/2/javascript_15.2.js
+++ FRONT/DYNAMIQUE/JS pour le web/15/2/javascript_15.2.js
@@ -1,4 +1,4 @@
-//
+
 //programme avec math random pour nombre aléatoire
 
 var NbMagique=parseInt(Math.random()*100);
@@ -10,7 +10,7 @@ var element = document.getElementById("button1");
 element.addEventListener("click", Verif);
 
 
-// Boules/conditions qui compare et affiche trop grd, trop petit
+// Compare et affiche trop grd, trop petit
 function Verif() {
 
     // initialise la variable ChiffreUtilisateur, qui est dans le doc, dans le formulaire (forms), qui est un éléments, avec un id "textBox1"
@@ -20,20 +20,20 @@ var ChiffreUtilisateur = document.forms["NombreMagique"].elements["textBox1"].va
     console.log("vous avez saisi le chiffre : " + ChiffreUtilisateur);
 
     if(ChiffreUtilisateur<NbMagique){
-        var info = document.getElementById('label1').innerHTML="Trop Petit !";
+        var info = document.getElementById('label1').innerHTML=("<p> Trop Petit ! </p>");
         // alert(info);
     }
     else if(ChiffreUtilisateur>NbMagique){
-        var info= document.getElementById('label1').innerHTML='Trop grand !';
+        var info= document.getElementById('label1').innerHTML=("<p> Trop Grand ! </p>");
         // alert(info);
     }
     else{
-        var info= document.getElementById('label1').innerHTML='Bravo !';
+        var info= document.getElementById('label1').innerHTML=(' <p> Bravo ! </p>');
         // alert(info);
     }
 }
 
 // faire ne sorte que bouton entrée lance le clic : 
-//input.addEventListener('keydown', (e) => {if (e.keyCode == 13) doSomething()});
+// input.addEventListener('keydown', (e) => {if (e.keyCode == 13) doSomething()});
 
-// à tester !!!
+// j'sais pas comment le mettre en place, peut on avoir plusieurs événements ?
