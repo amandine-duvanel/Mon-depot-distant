17a0d57cf73ea07e277667c2e3c1bd752e126109
amandine-duvanel <87307030+amandine-duvanel@users.noreply.github.com>
2021-07-30 12:22:59 +0200

    js 16 commencé

---
+++ FRONT/DYNAMIQUE/JS pour le web/16. formulaires/javascript_16.js
@@ -0,0 +1,21 @@
+
+// La "Société" doit comporter au moins 1 caractère.
+
+// La "Personne à contacter" doit comporter au moins 1 caractère.
+
+// Le "Code postal" doit comporter 5 caractères numériques.
+
+// La "Ville" doit comporter au moins 1 caractère.
+
+// Le Email doit comporter au moins le caractère "@".
+
+
+
+// Le champ "Environnement technique" est une liste déroulante dans laquelle on peut choisir les techniques utilisées pour le projet proposé, ainsi qu'une zone de texte qui reçoit la sélection.
+
+// Il est permis de rajouter des informations manuelles dans la zone de texte.
+
+// La valeur "Choisissez" ne doit pas s'afficher dans la zone de texte si elle a été sélectionnée.
+
+// Prévoyez les messages appropriés en cas d'erreur de saisie :
+
