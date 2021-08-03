e59e9991ac62277650e909d1e2a2d4a77d8dd038
amandine-duvanel <87307030+amandine-duvanel@users.noreply.github.com>
2021-08-03 12:12:30 +0200

    js 16 beug regex

--- FRONT/DYNAMIQUE/JS pour le web/16. formulaires/javascript_16.js
+++ FRONT/DYNAMIQUE/JS pour le web/16. formulaires/javascript_16.js
@@ -1,13 +1,84 @@
 
-// La "Société" doit comporter au moins 1 caractère.
+//Déclaration des variables
+var societe = document.getElementById("nomSociete");
+var acontacter = document.getElementById("pContact");
+var adresse = document.getElementById("adrEntreprise")
+var postal = document.getElementById("codePostal");
+var Ville = document.getElementById("ville");
+var Email = document.getElementById("email");
+var tel = document.getElementById('telephone');
+var environnement = document.getElementById("envirProp");
 
-// La "Personne à contacter" doit comporter au moins 1 caractère.
+var formulaire = document.getElementById("formulr");
 
-// Le "Code postal" doit comporter 5 caractères numériques.
+//Déclaration des expressions régulières
 
-// La "Ville" doit comporter au moins 1 caractère.
+    //  La "Société" doit comporter au moins 1 caractère.
+    var filtreSociete = new RegExp("^[a-zA-Z0-9]+$");
 
-// Le Email doit comporter au moins le caractère "@".
+    // La "Personne à contacter" doit comporter au moins 1 caractère.
+    var filtreContact = new RegExp ("^[a-zA-Z0-9]+$");
+
+    // Le "Code postal" doit comporter 5 caractères numériques.
+    var filtrePostal = new RegExp ("^[0-9]{5}$");
+
+    // La "Ville" doit comporter au moins 1 caractère.
+    var filtreVille = new RegExp("^[a-zA-Z0-9]+$");
+
+    // Le Email doit comporter au moins le caractère "@".
+    var filtreEmail = new RegExp("^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$");
+
+
+
+
+ // envoyer le formulaire 
+document.getElementById("formulr").addEventListener("submit",function(e) {
+    // e.preventDefault();
+
+    // Execution des filtres
+    var resultSociete = filtreSociete.test(societe);
+    console.log("Input société vaut" + resultSociete);
+
+    var resultContact = filtreContact.test(acontacter);
+    console.log("Input contact vaut " + resultContact);
+
+    var resultPostal = filtrePostal.test(postal);
+    console.log("Input posal vaut " + resultPostal);    
+
+    var resultVille = filtreVille.test(Ville);
+    console.log("Input ville vaut " + resultVille);    
+
+    var resultEmail = filtreEmail.test(Email);
+    console.log("Input email vaut " + resultEmail);    
+
+    // définition variables des span erreur
+    var erreurSociete = document.getElementById("erreur1");
+    var erreurContact = document.getElementById("erreur2");
+    // var erreurPostal = document.getElementById("erreur3");
+    // var erreurVille = document.getElementById("erreur4");
+    // var erreurEmail = document.getElementById("erreur5");
+
+
+    if(resultSociete == false){
+        e.preventDefault();
+        erreurSociete.textContent ="Menteur!";
+        erreurSociete.style.color ="red";
+    }
+
+    if(resultContact == false){
+        e.preventDefault();
+        erreurContact.textContent ="Essaye encore!";
+        erreurContact.style.color ="red";
+    }
+
+       else { alert("fucking formulaire envoyé!");
+    };
+
+});
+
+
+// Probleme de reg ex ??? !!!
+
 
 
 
@@ -19,3 +90,4 @@
 
 // Prévoyez les messages appropriés en cas d'erreur de saisie :
 
+//coder le bootrap pour le visuel !!!
